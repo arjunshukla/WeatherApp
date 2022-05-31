57a42ec357769e64343dd8bfed13ddb8aaf05ff4
Arjun Shukla <arjun.shukla@outlook.com>
2022-05-30 22:24:20 -0700

    Add tests

---
+++ WeatherTests/ListViewModelTests.swift
@@ -0,0 +1,15 @@
+//
+//  ListViewModelTests.swift
+//  WeatherTests
+//
+//  Created by Arjun Shukla on 5/30/22.
+//
+
+import Foundation
+import XCTest
+@testable import Weather
+
+class ListViewModelTests: XCTestCase {
+    func testCellModelCreation() {}
+    func testCitySorting() {}
+}
